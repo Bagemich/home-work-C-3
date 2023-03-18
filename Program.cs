eb36039fbfaefb86c759da1c2d11ec422909d3a2
znako <127133685+Bagemich@users.noreply.github.com>
2023-03-18 14:43:20 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int num, r = 0, sum=0, t = 0;
+Console.Write("Введите пятизначное число: ");
+num = Convert.ToInt32(Console.ReadLine());
+if(num>9999 && num<100000)
+{
+for(t=num;num!=0;num=num/10);
+}
+r=num % 10;
+sum=sum*10+r;
+{
+if(t==sum)
+Console.Write($"{r} палиндром.");
+else
+Console.Write($"{t} не палиндром");
+}
+
+
+
+
+
