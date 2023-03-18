8e3e915ff47e5faa66fef2ee8318236674d8dd43
znako <127133685+Bagemich@users.noreply.github.com>
2023-03-18 14:45:07 +0500

    Add files via upload

---
+++ 3 Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Кубы:\n");
+for (int i=1;i<=n;i++)
+{
+    int k = i*i*i;
+    Console.Write($"{k}, ");
+}
+
